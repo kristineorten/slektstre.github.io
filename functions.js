a1716905744078650754ffc302a5b72a97f697e6
Kristine F. Orten <kristine.orten@gmail.com>
2021-10-11 18:50:56 +0200

    Changed to my own search formula

--- functions.js
+++ functions.js
@@ -1,15 +1,39 @@
 // Functions for name
 function getName_Full(id) {
-    return people[id].firstName+" "+people[id].middleName+" "+people[id].lastName;
+    var name = people[id].firstName+" "+people[id].middleName+" "+people[id].lastName;
+    return name.trim();
 }
 function getName_AlmostFull(id) {
-    return people[id].firstName+" "+people[id].middleNameShort+" "+people[id].lastName;
+    var name = people[id].firstName+" "+people[id].middleNameShort+" "+people[id].lastName;
+    return name.trim();
 }
 function getName_FirstAndMiddleShort(id) {
-    return people[id].firstName+" "+people[id].middleNameShort;
+    var name = people[id].firstName+" "+people[id].middleNameShort;
+    return name.trim();
 }
 
 // Functions for images
 function getMainImg(id) {
     return people[id].img_url[0];
+}
+
+// Functions for years: birth and died
+function getYears_BornAndDied(id) {
+    var out = "";
+    if (people[id].yearBorn != 0) {
+        out += people[id].yearBorn
+    } else {
+        out += "?"
+    }
+    out += " - "
+    if (people[id].dead) {
+        if (people[id].yearDied != 0) {
+            out += people[id].yearDied
+        } else {
+            out += "?"
+        }
+    } else {
+        out += "d.d."
+    }
+    return out
 }
