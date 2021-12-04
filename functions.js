cfa336ce59197a14db543d187991fb9422c1dd5d
Kristine F. Orten <kristine.orten@gmail.com>
2021-12-04 16:11:56 +0100

    New icon-code + changed tree

--- functions.js
+++ functions.js
@@ -1,39 +1,72 @@
 // Functions for name
 function getName_Full(id) {
+    if (id === null) {
+        return null;
+    }
     var name = people[id].firstName+" "+people[id].middleName+" "+people[id].lastName;
     return name.trim();
 }
 function getName_AlmostFull(id) {
+    if (id === null) {
+        return null;
+    }
+    //console.log("id"+id)
     var name = people[id].firstName+" "+people[id].middleNameShort+" "+people[id].lastName;
+    //console.log("name: "+name)
     return name.trim();
 }
 function getName_FirstAndMiddleShort(id) {
+    if (id === null) {
+        return null;
+    }
     var name = people[id].firstName+" "+people[id].middleNameShort;
     return name.trim();
 }
 
 // Functions for name based on tree-placement
 function treeId_to_Id(treeId) {
-    for (let i = 2; i < people.length; i++) {
-        if (people[i].tree_id == treeId) {
+    for (let i = 0; i < people.length; i++) {
+        if (people[i].tree_id.valueOf() == treeId.valueOf()) {
             return i;
         }
     }
     return null;
 }
 function getName_Full_treeId(treeId) {
-    return getName_Full(treeId_to_Id(treeId));
+    var id = treeId_to_Id(treeId);
+    if (id === null) {
+        return null;
+    }
+    return getName_Full(id);
 }
 function getName_AlmostFull_treeId(treeId) {
-    return getName_AlmostFull_treeId(treeId_to_Id(treeId));
+    var id = treeId_to_Id(treeId);
+    if (id === null) {
+        return null;
+    }
+    return getName_AlmostFull(id);
 }
 function getName_FirstAndMiddleShort_treeId(treeId) {
-    return getName_FirstAndMiddleShort_treeId(treeId_to_Id(treeId));
+    var id = treeId_to_Id(treeId);
+    if (id === null) {
+        return null;
+    }
+    return getName_FirstAndMiddleShort(id);
 }
 
 // Functions for images
 function getMainImg(id) {
+    if (id === null || people[id].img_url === null) {
+        return null;
+    }
     return people[id].img_url[0];
+}
+function getMainImg_treeId(treeId) {
+    var id = treeId_to_Id(treeId);
+    if (id === null) {
+        return null;
+    }
+    return getMainImg(id);
 }
 
 // Functions for years: birth and died
@@ -55,4 +88,16 @@ function getYears_BornAndDied(id) {
         out += "d.d."
     }
     return out
+}
+
+//Check if dead
+function isDead(id) {
+    if (id === null) {
+        return null;
+    }
+    return people[id].dead;
+}
+
+function isDead_treeId(tree_id) {
+    return isDead(treeId_to_Id(tree_id))
 }
